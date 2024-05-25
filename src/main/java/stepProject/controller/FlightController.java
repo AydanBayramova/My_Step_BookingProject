198a09a1f87718d5da40dba28b0c62cbd487c8dc
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-05-25 04:38:54 +0400

    Create FlightException

--- src/main/java/stepProject/controller/FlightController.java
+++ src/main/java/stepProject/controller/FlightController.java
@@ -1,4 +1,65 @@
 package stepProject.controller;
 
+import stepProject.exception.BookingException;
+import stepProject.model.dto.BookingDto;
+import stepProject.model.dto.FlightDto;
+import stepProject.model.entity.BookingEntity;
+import stepProject.model.entity.FlightEntity;
+import stepProject.service.FlightService;
+
+import java.util.List;
+import java.util.Optional;
+
 public class FlightController {
+    private final FlightService flightService;
+
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+    public FlightDto addFlight(FlightDto flightDto) {
+        if (flightDto.getFlightId() > 0) {
+            FlightEntity flightEntity = (FlightEntity) flightService.saveAll(flightDto);
+            return mapToDto(flightEntity);
+        } else {
+            throw new BookingException("Booking id can't be empty");
+        }
+    }
+
+    private FlightDto mapToDto(FlightEntity flightEntity) {
+        FlightDto flightDto = new FlightDto();
+        flightDto.setFlightId(flightDto.getFlightId());
+        flightDto.setLocations(flightDto.getLocations());
+        flightDto.setDepartureDate(flightDto.getDepartureDate());
+        flightDto.setFreeSeats(flightDto.getFreeSeats());
+        return flightDto;
+    }
+
+    public List<FlightEntity> getAll() {
+        if (flightService.getAll() != null) {
+            return flightService.getAll();
+        } else {
+            return null;
+        }
+
+
+    }
+
+    public Optional<FlightEntity> getById(Long id) {
+        if (id > 0 && flightService.getById(id).isPresent()) {
+            return flightService.getById(id);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public boolean deleteById(Long id) {
+        if (flightService.getById(id).isPresent()) {
+            return flightService.deleteById(id);
+        } else {
+            return false;
+        }
+    }
+
+
 }
---
+++ src/main/java/stepProject/exception/FlightException.java
@@ -0,0 +1,8 @@
+package stepProject.exception;
+
+public class FlightException extends RuntimeException {
+
+    public FlightException(final String message) {
+        super(message);
+    }
+}
