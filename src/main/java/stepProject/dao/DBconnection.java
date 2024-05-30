8d1eed991e400a8a5b48e433d1191fc4f3f554b5
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-05-30 23:33:51 +0400

    Create DB Connection

---
+++ src/main/java/stepProject/dao/DBconnection.java
@@ -0,0 +1,20 @@
+package stepProject.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","A");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+}
