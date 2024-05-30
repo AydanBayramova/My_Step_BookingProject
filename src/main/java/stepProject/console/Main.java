103fa9560f37c8a2ff5207bd3b1983b24bdcf75b
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-05-30 15:02:18 +0400

    Main class allocated with consoleApp class

--- src/main/java/stepProject/console/Main.java
+++ src/main/java/stepProject/console/Main.java
@@ -3,19 +3,22 @@ package stepProject.console;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import stepProject.dao.imp.BookingDaoInFile;
-import stepProject.dao.imp.FlightDaoiInFile;
+import stepProject.dao.imp.BookingDaoInMemory;
+import stepProject.dao.imp.FlightDaoInFile;
+import stepProject.dao.imp.FlightInMemory;
 import stepProject.service.BookingService;
 import stepProject.service.FlightService;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        FlightDaoiInFile flightDao = new FlightDaoiInFile(new ObjectMapper().registerModule(new JavaTimeModule()));
-        BookingDaoInFile bookingDaoInFile=new BookingDaoInFile(new ObjectMapper().registerModule(new JavaTimeModule()));
-
-        FlightService flightService = new FlightService(flightDao);
+        //  FlightInMemory flightInMemory = new FlightInMemory();
+        //  BookingDaoInMemory bookingDaoInMemory = new BookingDaoInMemory();
+        FlightDaoInFile flightDaoInFile = new FlightDaoInFile();
+        BookingDaoInFile bookingDaoInFile = new BookingDaoInFile();
+        FlightService flightService = new FlightService(flightDaoInFile);
         BookingService bookingService = new BookingService(bookingDaoInFile);
-
         ConsoleApp consoleApp = new ConsoleApp(flightService, bookingService);
         consoleApp.run();
     }
