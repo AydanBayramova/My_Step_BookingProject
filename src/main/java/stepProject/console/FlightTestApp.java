f2bb67d5a6394d45def096e10ce24232fa67bc99
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-05-30 23:37:35 +0400

    Allocate with DB and write database

--- src/main/java/stepProject/console/FlightTestApp.java
+++ src/main/java/stepProject/console/FlightTestApp.java
@@ -2,6 +2,7 @@ package stepProject.console;
 
 import stepProject.controller.FlightController;
 
+import stepProject.dao.FlightDaoDB;
 import stepProject.dao.imp.FlightDaoInFile;
 import stepProject.model.dto.FlightDto;
 import stepProject.service.FlightService;
@@ -12,17 +13,18 @@ public class FlightTestApp {
 
     public static void main(String[] args) {
 
-        FlightDaoInFile flightDaoInFile = new FlightDaoInFile();
+     //   FlightDaoInFile flightDaoInFile = new FlightDaoInFile();
         //  FlightInMemory  flightInMemory = new FlightInMemory();
-        FlightService flightService = new FlightService(flightDaoInFile);
+        FlightDaoDB flightDaoDB = new FlightDaoDB();
+        FlightService flightService = new FlightService(flightDaoDB);
         FlightController flightController = new FlightController(flightService);
         flightController.addFlight(new FlightDto(1L, "Baku", LocalDate.of(2024, 4, 2), 1L, "Kiyev"));
         flightController.addFlight(new FlightDto(2L, "Baku3", LocalDate.of(2024, 6, 2), 1L, "Kiyev"));
         flightController.addFlight(new FlightDto(3L, "Baku6", LocalDate.of(2024, 7, 2), 1L, "Kiyev"));
         flightController.addFlight(new FlightDto(4L, "Baku9", LocalDate.of(2024, 8, 2), 1L, "Kiyev"));
-        System.out.println(flightController.getAll());
-        System.out.println(flightController.deleteById(2L));
-         System.out.println(flightController.getById(3L));
+//        System.out.println(flightController.getAll());
+//        System.out.println(flightController.deleteById(2L));
+//         System.out.println(flightController.getById(3L));
 
 
     }
